269299650fa1ada29378cfcff39b2c35f84ec55d
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2025-07-20 23:12:09 +0330

    task created

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // اگر نمی‌خواهید خطاهای TypeScript در هنگام بیلد بررسی شوند
+  },
+};
+
+export default nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        glossy: "#1a1a1a",
+      },
+    },
+  },
+  plugins: [],
+};
